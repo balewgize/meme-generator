2463b32afcd267b75061fa97ac98d4c94173dd6b
Alemnew Marie <alemnewmarie461@gmail.com>
2024-04-05 09:23:38 +0300

    add form state object

--- src/components/MemeForm.jsx
+++ src/components/MemeForm.jsx
@@ -9,6 +9,14 @@ export default function Meme() {
     });
     const [allMemeImages, setAllMemeImages] = useState(memesData.data.memes);
 
+    const handleTextChange = (event) => {
+        const { name, value } = event.target;
+        setMeme((prevMeme) => ({
+            ...prevMeme,
+            [name]: value,
+        }));
+    }
+
     const getMemeImage = () => {
         // TODO: fetch memes data from API
         const randomIndex = Math.floor(Math.random() * allMemeImages.length);
@@ -24,20 +32,36 @@ export default function Meme() {
             <div className="form">
                 <div className="d-flex flex-column flex-lg-row p-2">
                     <div className="mb-2 mx-0 me-lg-2 w-100">
-                        <label htmlFor="top-text" className="form-label">Top text</label>
-                        <input type="text" className="form-control" id="top-text" placeholder="Shut up" required />
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="top-text"
+                            placeholder="Top text"
+                            onChange={handleTextChange}
+                            name="topText"
+                            value={meme.topText}
+                        />
                     </div>
                     <div className="mb-2 mx-0 ms-lg-2 w-100">
-                        <label htmlFor="bottom-text" className="form-label">Bottom text</label>
-                        <input type="text" className="form-control" id="bottom-text" placeholder="and take my money" required />
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="bottom-text"
+                            placeholder="Bottom text"
+                            onChange={handleTextChange}
+                            name="bottomText"
+                            value={meme.bottomText}
+                        />
                     </div>
                 </div>
                 <div className="p-2">
                     <button onClick={getMemeImage} className="btn btn-primary d-block w-100 custom-btn">Generate a new meme image ✅</button>
                 </div>
             </div>
-            <div className="text-center mt-3 mb-5">
-                <img src={meme.randomImage} className="rounded" />
+            <div className="position-relative d-flex justify-content-center mt-3 mb-5 meme">
+                <img src={meme.randomImage} className="rounded w-100 meme--image" />
+                <h2 className="position-absolute top-0 mt-5 text-white text-center meme--text">{meme.topText}</h2>
+                <h2 className="position-absolute bottom-0 mb-5 text-white text-center meme--text">{meme.bottomText}</h2>
             </div>
         </div>
     )
